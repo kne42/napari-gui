26dd02f30b35ab7dbe6f0b0f87cd52a20db424d3
Kira Evans <contact@kne42.me>
2019-08-23 11:44:53 -0700

    import base class subclasses so they are run

--- napari/_qt/layers/qt_image_layer.py
+++ napari/_qt/layers/qt_image_layer.py
@@ -2,10 +2,10 @@ from qtpy.QtWidgets import QHBoxLayout
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QLabel, QComboBox
 
-from ..layers import Image
-from ..layers.image._constants import Interpolation
+from ...layers import Image
+from ...layers.image._constants import Interpolation
 from .qt_base_layer import QtLayerControls, QtLayerProperties
-from . import QVRangeSlider
+from .. import QVRangeSlider
 
 
 class QtImageControls(QtLayerControls, layer=Image):
